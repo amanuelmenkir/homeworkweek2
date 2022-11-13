6b77629bd66738368d80622ca601b161d3934fd9
amanuelm <116317627+amanuelmenkir@users.noreply.github.com>
2022-11-13 00:27:33 -0500

    Add files via upload
    Adding Calculator Interface

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+package yearup.week2.homework;
+
+public interface Calculator {
+    //calculates the area with length and width
+    double calculateArea(double length, double width);
+
+    //calculates sum of two values
+    double calculateSum(double value1, double value2);
+
+    //calculates volume
+    double calculateVolume(double length, double width, double height);
+}
+
+
